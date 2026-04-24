931232707000151fb25b704e1d65b50686c9f680
Christian Xavier <christianxavier774@gmail.com>
2026-04-24 08:02:37 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        navy: {
+          950: "#070e22",
+          900: "#0D1B3E",
+          800: "#1a2a52",
+          700: "#243566",
+        },
+        "amh-red": "#D32F2F",
+        "amh-blue": "#3A4A6B",
+        "amh-gray": "#F1F3F7",
+        "amh-muted": "#9CA3AF",
+      },
+      fontFamily: {
+        display: ["Bebas Neue", "cursive"],
+        sans: ["Inter", "sans-serif"],
+      },
+    },
+  },
+};
